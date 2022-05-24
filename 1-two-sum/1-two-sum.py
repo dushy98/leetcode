cd623054336cba6ff08c3045c9938e34129e34c3
dushy98 <dushy98@gmail.com>
2022-05-24 20:42:24 +0530

    Time: 102 ms (50.92%), Space: 15.5 MB (9.27%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i in range(len(nums)):
+            if nums[i] in dict:
+                return [dict[nums[i]], i]
+            
+            else:
+                dict[target - nums[i]] = i
+        
