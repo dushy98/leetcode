fc9b00d1d5e05503249ef4fd788a1a4140d792af
dushy98 <dushy98@gmail.com>
2022-05-24 23:48:33 +0530

    Time: 67 ms (5.01%), Space: 13.7 MB (96.64%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # using top-down memoization
+        
+        memo = {}
+        memo[1] = 1
+        memo[2] = 2
+        
+        def climb(n):
+            if n in memo:           # check if recusion done before in look-up table
+                return memo[n]
+            else:                   # store the recusrssion function in look-up table and return
+                memo[n] = climb(n-1) + climb(n-2)
+                return memo[n]
+            
+        return climb(n)
+            
+        
+
+        
