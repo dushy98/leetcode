988266b1fd830fb55d12bee3328b945876b1dc62
dushy98 <dushy98@gmail.com>
2022-05-26 00:06:28 +0530

    Time: 70 ms (66.60%), Space: 13.8 MB (99.90%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,24 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # check input
-        if not cost:
-            return 0
+        cost.append(0)
         
-        # memo to store results for each index
-        dp = [0] * len(cost)
+        for i in range(len(cost) -3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
         
-        dp [0] = cost[0]
-        
-        # dp 1 will be 1
-        if len(cost) >= 2:
-            dp[1] = cost[1]
-        
-        # since we can come only from the previous 2 stairs to this stair 
-        # we add the minimum of these 2 to the cost 
-        for i in range(2, len(cost)):
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-        
-        #similarly the last step can be stepped on from previous 2 steps only
-        # hence we return minimum of them
-        return min(dp[-1], dp[-2])
+        return min(cost[0], cost[1])
+
         
