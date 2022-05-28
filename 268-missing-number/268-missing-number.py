e0ac3d441bc3a714b67d2b9fa4408fe1f6eb91e9
dushy98 <dushy98@gmail.com>
2022-05-28 18:54:58 +0530

    Time: 170 ms (61.65%), Space: 15.1 MB (98.72%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # one line O(1) solution using maths
         return int(len(nums) * (len(nums) + 1) / 2 - sum(nums))
         
 
