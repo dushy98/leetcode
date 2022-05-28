84ff21fa4d9e04049120bbe0c6b0466b890176f5
dushy98 <dushy98@gmail.com>
2022-05-28 21:12:08 +0530

    Time: 51 ms (30.82%), Space: 13.8 MB (66.25%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,17 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # 1. construct dp table 
-        if not nums:
-            return 0
-        
-        if len(nums) == 1:
-            return nums[0]
-        
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
-        
-        for i in range(2, len(nums)):
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
-        return dp[-1] # return the last element of dp table
-        
+        # 2. constant space using 2 variables
+        prev = curr = 0
+        for num in nums:
+            prev, curr = curr, max(num + prev, curr)
+        return curr
