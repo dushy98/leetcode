36af0a18ac30e272719f8dcb7dae7b8a2ea4f854
dushy98 <dushy98@gmail.com>
2022-05-29 20:41:42 +0530

    Time: 524 ms (80.38%), Space: 14.3 MB (71.68%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        d, ans = defaultdict(int), 0
+        
+        for word in words:
+            for l in word:
+                d[word] |= 1<<(ord(l) - 97)
+        
+        for w1, w2 in combinations(d.keys(), 2):
+            if d[w1] & d[w2] == 0:
+                ans = max(ans, len(w1) * len(w2))
+        
+        return ans
+                
+        
