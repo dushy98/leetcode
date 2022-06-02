1c7bbe4b727408ee2157e2d76fd7e3ff6c8ba6f4
dushy98 <dushy98@gmail.com>
2022-06-02 21:20:43 +0530

    Time: 99 ms (50.08%), Space: 14.6 MB (90.58%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.py
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -1,6 +1,6 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        # made the 1-liner more read-able
+        # made the 1-liner more readable
         return [[matrix[i][j] 
                     for i in range(len(matrix))] 
                         for j in range(len(matrix[0]))]
