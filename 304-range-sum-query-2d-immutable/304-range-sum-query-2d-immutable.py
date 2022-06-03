59e0ec7343903f354d05057668563fb2b27bc404
dushy98 <dushy98@gmail.com>
2022-06-03 23:13:50 +0530

    Time: 2795 ms (25.08%), Space: 24.9 MB (55.21%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -3,7 +3,7 @@ class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
         self.dp = [[0] * (len(matrix[0])+1) for _ in range(len(matrix) + 1)]
         
-        # prefix sum calculation
+        # prefix sum calculation :-
         for r in range(len(self.dp)-1):
             for c in range(len(self.dp[0])-1):
                 self.dp[r+1][c+1] = matrix[r][c] + self.dp[r][c+1] + self.dp[r+1][c] - self.dp[r][c]
