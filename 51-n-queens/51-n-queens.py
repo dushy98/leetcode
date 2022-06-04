dcfe7696f75ca5ec2275e3fc933c0397b1b527bd
dushy98 <dushy98@gmail.com>
2022-06-04 18:50:33 +0530

    Time: 256 ms (10.79%), Space: 14.2 MB (95.08%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,26 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        self.dfs([-1]*n, 0, [], res)
+        return res
+    
+    def dfs(self, nums, index, path, res):
+        if index == len(nums):             
+            res.append(path)
+            return                          # Backtracking
+        
+        for i in range(len(nums)):
+            nums[index] = i
+            if self.valid(nums, index):
+                tmp = "."*len(nums)
+                self.dfs(nums, index+1, path + [tmp[:i] + "Q" + tmp[i+1:]], res)
+                
+    def valid(self, nums, n):
+        for i in range(n):
+            if abs(nums[i]-nums[n]) == n-i or nums[i] == nums[n]:
+                return False
+        return True
+                
+                
+        
+        
