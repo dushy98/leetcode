2ebc223cfbb21267f57fe71f36dcabb1fd09eb09
dushy98 <dushy98@gmail.com>
2022-06-07 10:41:56 +0530

    Time: 68 ms (18.52%), Space: 13.9 MB (85.45%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # merge operation from the last of both lists
+        # merge operation from the ends and saving the result from last of list1
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[m + n -1] = nums1[m-1]
