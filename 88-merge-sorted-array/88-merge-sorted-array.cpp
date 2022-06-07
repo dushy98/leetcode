28aed0362173dc3fae98e112b001620749f9c440
dushy98 <dushy98@gmail.com>
2022-06-07 10:58:26 +0530

    Time: 4 ms (49.07%), Space: 9.2 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m - 1, j = n - 1, k = m + n - 1; 
         while( i >= 0 and j >= 0){
-            if (nums1[i] > nums2[j]){
+            if (nums1[i] > nums2[j]){        // compare last elements of the two and move the bigger towards the end of nums1
                 nums1[k--] = nums1[i--];
             }
             else{
