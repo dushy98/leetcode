833cc14ee292d7ec3aaa7ae90fefb46efbaec0b1
dushy98 <dushy98@gmail.com>
2022-06-08 20:35:48 +0530

    Time: 47 ms (29.56%), Space: 13.9 MB (10.77%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
@@ -1,5 +1,5 @@
 class Solution:
     def removePalindromeSub(self, s: str) -> int:
-        #difference between substring and subsequence
+        
         return 2 - (s == s[::-1]) - (s == "")
         
