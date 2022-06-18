68b60391559315333ea6a3111a377d5ff56b72fe
Dushyant Pal <dushy98@gmail.com>
2022-06-18 16:55:30 +0530

    Time: 110 ms (8.04%), Space: 13.8 MB (55.58%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -4,7 +4,7 @@ class Solution:
         for i in operations:
             if i == "--X" or i == "X--":
                 x -= 1
-            elif i == "++X" or i == "X++":
+            else:
                 x += 1
                 
         return x
