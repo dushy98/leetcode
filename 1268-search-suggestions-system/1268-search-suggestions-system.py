7f0c6b057426c11f55e390f18c2a003dc72bf89c
Dushyant Pal <dushy98@gmail.com>
2022-06-19 19:29:10 +0530

    Time: 138 ms (72.03%), Space: 17.1 MB (58.95%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,11 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
+        res, prefix, i = [], '', 0
+        for c in searchWord:
+            prefix += c
+            i = bisect.bisect_left(products, prefix, i)
+            res.append([w for w in products[i:i+3] if w.startswith(prefix)])
+        return res
+        
+        
