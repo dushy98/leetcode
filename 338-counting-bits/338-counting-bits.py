502251d5c183d211a60d064b99d7d0a2651d57b7
Dushyant Pal <dushy98@gmail.com>
2022-06-30 23:03:03 +0530

    Time: 91 ms (87.70%), Space: 20.8 MB (28.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,7 +1,15 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        counter = [0]
+        nextOrder = 2
+        tracker = 0
+        counter = [0]*(n + 1)
+        
         for i in range(1, n+1):
-            counter.append(counter[i >> 1] + i % 2)
+            if i == nextOrder:
+                nextOrder *= 2
+                tracker = 0
+            counter[i] = counter[tracker] + 1
+            tracker += 1
         return counter
+
         
