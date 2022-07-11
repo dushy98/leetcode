5c1427967a3a1dac34a2e3cd694570267f2f0f5d
Dushyant Pal <dushy98@gmail.com>
2022-07-11 18:15:58 +0530

    Time: 45 ms (64.48%), Space: 14 MB (22.93%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -6,13 +6,11 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        def collect(node, depth):
-            if node:
-                if depth == len(view):
-                    view.append(node.val)
-                collect(node.right, depth+1)
-                collect(node.left, depth+1)
         view = []
-        collect(root, 0)
+        if root:
+            level = [root]
+            while level:
+                view += level[-1].val,
+                level = [child for node in level for child in (node.left, node.right) if child]
         return view
         
