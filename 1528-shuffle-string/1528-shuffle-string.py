0dcb866a85efea5c21cf311f9bd77cd51fcfcfcd
Dushyant Pal <dushy98@gmail.com>
2022-07-16 23:25:16 +0530

    Time: 69 ms (76.96%), Space: 14 MB (15.52%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        res = [''] * len(s)
+        for index, char in enumerate(s):
+            res[indices[index]] = char
+            
+        return "".join(res)
