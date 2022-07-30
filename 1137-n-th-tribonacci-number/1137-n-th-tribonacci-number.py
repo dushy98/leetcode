81b16807ca0aec910e9804ceeff04a6aeb2d3581
Dushyant Pal <dushy98@gmail.com>
2022-07-30 15:47:02 +0530

    Time: 51 ms (36.01%), Space: 13.9 MB (11.71%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        dp = [0, 1, 1]
-        for i in range(3,n+1):
-            dp[i % 3] = sum(dp)
-        return dp[n % 3]
-        
+        a, b, c = 1, 0, 0
+        for _ in range(n):
+            a, b, c = b, c, a+b+c
+        return c
