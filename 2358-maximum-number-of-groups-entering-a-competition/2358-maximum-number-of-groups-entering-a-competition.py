c339a31a5a60fb082c7b8af3346ba103ac8cc502
Dushyant Pal <dushy98@gmail.com>
2022-08-03 22:13:30 +0530

    Time: 848 ms (58.17%), Space: 27.4 MB (36.53%) - LeetHub

---
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumGroups(self, grades: List[int]) -> int:
+        n = len(grades)
+        k = 0
+        
+        while n >= k + 1:
+            k += 1
+            n -= k
+        return k
