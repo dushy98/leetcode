589f551a40aaa2e8f0987f535146194c73a01881
Dushyant Pal <dushy98@gmail.com>
2022-08-14 09:53:57 +0530

    Time: 301 ms (75.00%), Space: 14.4 MB (25.00%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
@@ -5,14 +5,14 @@ class Solution:
         
         for i in range(N-2):
             for j in range(N-2):
-                maxV =  max(grid[i+1][j+1],
+                maxV =  max(grid[i][j],
                             grid[i][j+1],
-                            grid[i+2][j+1],
-                            grid[i+1][j],
+                            grid[i+1][j+1],
                             grid[i+1][j+2],
-                            grid[i][j],
+                            grid[i+1][j],
                             grid[i+2][j+2],
-                            grid[i][j+2], 
+                            grid[i][j+2],
+                            grid[i+2][j+1],
                             grid[i+2][j])
                 
                 ans[i][j] = maxV
