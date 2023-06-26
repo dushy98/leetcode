aa4d46f1cd2cd106a7ed4dc50c681a86806ae851
Dushyant Pal <dushy98@gmail.com>
2023-06-26 09:42:29 +0530

    Time: 65 ms (96.46%), Space: 18 MB (5.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,7 +4,5 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] in dict:
                 return [dict[nums[i]], i]
-            
             else:
                 dict[target - nums[i]] = i
-        
