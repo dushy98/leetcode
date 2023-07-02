a59e26f983fba0226e117e6c13e5c5c9556e8883
Dushyant Pal <dushy98@gmail.com>
2023-07-02 22:18:38 +0530

    Time: 541 ms (78.58%), Space: 31.8 MB (40.08%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,9 +1,5 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         #comment to test autocommit
-        freq = Counter(nums)
-        for num, freq in freq.items():
-            if freq > 1:
-                return True
-        return False
+        return len(set(nums)) != len(nums)
         #return True if len(nums) != len(set(nums)) else False
