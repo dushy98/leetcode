7f6517bec09f5d6cb3803c318ee4071e2fd24dd8
Dushyant Pal <dushy98@gmail.com>
2023-07-04 22:52:56 +0530

    Time: 147 ms (26.7%), Space: 18.1 MB (61.98%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,13 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        #brute force - approach 1
-        count = defaultdict(int)
+        #Bit Manipulation - approach 2
+        ans = 0
 
-        for i in nums:
-            count[i] += 1
-        
-        for i, freq in count.items():
-            if freq == 1:
-                return i
-        
-        return -1
+        for i in range(32):
+            bit_sum = 0
+            for num in nums:
+                # Convert the number to 2's complement for large test cases
+                if num < 0:
+                    num = num&(2**32-1)
+                bit_sum += (num >> i) & 1
+            bit_sum %= 3 
+            ans |= bit_sum << i
+
+        if ans >= 2**31:
+            ans -= 2**32
+            
+        return ans
